1d4da247cf2780ac6dd0fef163cb0aef3edbb47a
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-29 18:06:48 -0700

    Start adding data

---
+++ restaurantsData.js
@@ -0,0 +1,282 @@
+export const locations = [
+    {
+        city: 'Berlin',
+        avgVegans: '',
+        restaurants: [
+            {
+                name: 'Cafe Vux',
+                restaurantType: 'Coffee shop',
+                address: 'Wipperstr 14 (at Kirchhofstrasse), Berlin, Germany, 12055'
+            },
+            {
+                name: 'Brammibals Donuts - Potsdamer Platz',
+                restaurantType: 'Donut Shop',
+                address: 'Alte Potsdamer Straße 7, Berlin, Germany, 10785'
+            },
+            {
+                name: 'Vaust Braugaststatte',
+                restaurantType: 'Restaurant',
+                address: 'Pestalozzistrasse 8, Berlin, Germany, 10625'
+            },
+            {
+                name: 'Quy Nguyen - Vegan Living',
+                restaurantType: 'South Vietnamese vegan cuisine restaurant',
+                address: 'Oranienburger Str. 7, Berlin, Germany, 10178'
+            },
+            {
+                name: 'Kopps',
+                restaurantType: 'Fine dining vegan restaurant',
+                address: 'Linienstr 94, Berlin, Germany, 10115'
+            },
+            {
+                name: 'La Stella Nera',
+                restaurantType: 'Vegan pizzeria',
+                address: 'Leykestraße 18, Berlin, Germany, 12053'
+            },
+            {
+                name: 'Cat Tuong',
+                restaurantType: 'Vietnamese vegan cuisine ',
+                address: 'Kastanienallee 89, Berlin, Germany, 10435'
+            },
+            {
+                name: 'Lucky Leek',
+                restaurantType: 'gourmet cuisine',
+                address: 'Kollwitzstr 54, Berlin, Germany, 10405'
+            },
+            {
+                name: 'Element Five',
+                restaurantType: 'Vegan tapas gastrobar serving Asian fusion',
+                address: 'Skalitzerstrasse 46b (at Spreewaldplatz), Berlin, Germany, 10997'
+            },
+            {
+                name: 'Veg room',
+                restaurantType: 'Vegan cafe',
+                address: 'Triftstrasse 37, Berlin, Germany, 13353'
+            }
+        ]
+    },
+    {
+        city: 'London',
+        avgVegans: '',
+        restaurants: [
+            {
+                name: 'Purezza',
+                restaurantType: 'Vegan Italian restaurant',
+                address: '43 Pkwy, Camden, North West London, England, NW1 7PN'
+            },
+            {
+                name: 'Biffs Jack Shack - Dalston',
+                restaurantType: 'All-vegan pub',
+                address: '33-35 Stoke Newington Rd, North London, England, N16 8BJ'
+            },
+            {
+                name: 'Cafe Forty One',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: 'Cafe Forty One',
+                restaurantType: 'Cafe',
+                address: '41-51 Inverness Terrace, Bayswater (at La Suite West Hotel), West London, England, W2 3JN'
+            },
+            {
+                name: 'Essential Vegan Cafe',
+                restaurantType: 'Brazilian-inspired vegan cafe',
+                address: '6 Calvert Ave, East London, England, E2 7JP'
+            },
+            {
+                name: 'Ethiopiques - South Bank',
+                restaurantType: 'Vegan Ethiopian',
+                address: 'South Bank Food Market, Waterloo, East London, England'
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            }
+        ]
+    },
+    {
+        city: 'Amsterdam',
+        avgVegans: '',
+        restaurants: [
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            }
+        ]
+    },
+    {
+        city: 'Bangkok',
+        avgVegans: '',
+        restaurants: [
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            }
+        ]
+    },
+    {
+        city: 'Israel',
+        avgVegans: '',
+        restaurants: [
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            },
+            {
+                name: '',
+                restaurantType: '',
+                address: ''
+            }
+        ]
+    }
+]
